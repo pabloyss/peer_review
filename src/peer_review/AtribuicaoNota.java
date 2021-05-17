e18ae9e85ae6f47d47463d6b342cd465cb88076d
Pablo Yuri Scherer de Souza <pabloyuriss@hotmail.com>
2021-05-16 23:53:17 -0300

    a

--- src/peer_review/AtribuicaoNota.java
+++ src/peer_review/AtribuicaoNota.java
@@ -10,6 +10,7 @@ public class AtribuicaoNota implements OpcoesCommand {
 		Database db = new Database(false);
 		Collection<Artigo> artigos;
 		Collection<Pesquisador> revisores;
+		int nota;
 		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
 
 		artigos = db.pegaTodosArtigos();
@@ -29,6 +30,11 @@ public class AtribuicaoNota implements OpcoesCommand {
 
 		System.out.prinlnt("\nEscolha o ID do revisor que irá avaliar")
 		int artigo_revisor = Integer.parseInt(reader.readLine());
+
+		System.out.prinlnt("\Insira uma nota de -3 a 3")
+		int nota = Integer.parseInt(reader.readLine());
+
+		Artigo artigo = artigos.get(artigo_escolhido - 1);
 
 		
 	}
